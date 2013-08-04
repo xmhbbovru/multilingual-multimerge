86e11a1a4ee15587ddf36d9db6137b2e41cbe5ec
xmhbbovru <kidledivydo-7@yahoo.com>
2013-08-04 06:12:07 -0700

    Changed timing graphs and comments

--- common/analyze.R
+++ common/analyze.R
@@ -1,12 +1,14 @@
-# common/analyze.R rev. 31 July 2013 by Stuart Ambler.  analyze function.
+# common/analyze.R rev. 04 August 2013 by Stuart Ambler.  analyze function.
 # Copyright (c) 2013 Stuart Ambler.
 # Distributed under the Boost License in the accompanying file LICENSE
 
 # Fits a choice of log(k) * n or k * n (governed by use.log TRUE or FALSE)
 # to table t (read from testdata.txt) with rows selected by ixs.  method
 # is text inserted into filenames and graph labels ("pq" or "lin" for now,
-# and this also selects the dependent variable column in the table).
-analyze <- function(t, ixs, method, use.log) {
+# and this also selects the dependent variable column in the table).  dir
+# is a string prepended to the title; commonanalyze sets it to the language
+# directory where the data is being analyzed.
+analyze <- function(t, ixs, method, use.log, dir) {
   sel  <- t[ixs,]
   len  <- length(ixs)
   ktxt <- "k"
@@ -17,9 +19,22 @@ analyze <- function(t, ixs, method, use.log) {
   }
   lmresult <- lm(sel[,method] ~ I(kfun(sel$k) * sel$n))
   pdf(sprintf("%s%g.pdf", method, len))
-  plot(lmresult$coeff[1] + lmresult$coeff[2] * kfun(sel$k) * sel$n,
-       sel[,method], xlab=sprintf("%g points fitted a + b * %s * n", len, ktxt),
-       ylab=method, main=sprintf("%s timings vs. fitted formula", method))
+  pred <- function(k,n) { return(  lmresult$coeff[1]
+                                 + lmresult$coeff[2] * kfun(k) * n) }
+  x = pred(sel$k, sel$n)
+  y = sel[,method]
+  lim = c(min(min(x), min(y)), max(max(x), max(y)))
+  mineach = min(sel$each)
+  maxeach = max(sel$each)
+  if (mineach == maxeach)
+    plotchars = 79
+  else
+    plotchars = 79 + 11 * (sel$each - mineach) / (maxeach - mineach)
+  plot(x, y, xlim=lim, ylim=lim, pch=plotchars,
+       xlab=sprintf("%g points fitted a + b * %s * n, %s",
+                    len, ktxt, "O..Z for ave input len range"),
+       ylab=method,
+       main=sprintf("%s %s timings vs. fitted formula", dir, method))
   dev.off()
   cat(sprintf("%g points %s  ~= %11.3e + %11.3e * %s * n\n",
               len, method, lmresult$coeff[1], lmresult$coeff[2], ktxt))
--- common/commonanalyze.R
+++ common/commonanalyze.R
@@ -1,9 +1,10 @@
 #!/usr/bin/env Rscript
-# common/commonanalyze.R rev. 03 August 2013 by Stuart Ambler.
+# common/commonanalyze.R rev. 04 August 2013 by Stuart Ambler.
 # Copyright (c) 2013 Stuart Ambler.
 # Distributed under the Boost License in the accompanying file LICENSE
 
 source('../common/analyze.R')
+dir = basename(getwd())
 pq  = "pq"
 lin = "lin"
 use.log=list()
@@ -12,7 +13,7 @@ use.log[lin] = FALSE
 t<-read.table('testdata.txt',header=TRUE)
 t$k = 1.0 * t$k  # to avoid integer overflow of product
 t$n = 1.0 * t$n  # to avoid integer overflow of product
-analyze(t, c(seq(1,17)), pq, use.log[pq][[1]])
-analyze(t, c(seq(1,11),13), lin, use.log[lin][[1]])
-analyze(t, seq(1,11), pq, use.log[pq][[1]])
-analyze(t, seq(1,11), lin, use.log[lin][[1]])
+analyze(t, c(seq(1,17)), pq, use.log[pq][[1]], dir)
+analyze(t, c(seq(1,11),13), lin, use.log[lin][[1]], dir)
+analyze(t, seq(1,11), pq, use.log[pq][[1]], dir)
+analyze(t, seq(1,11), lin, use.log[lin][[1]], dir)
